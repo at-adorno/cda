937698a2e7b8ec4f4cdf27e4d1fb7054ea4b84d0
André Tavares Adorno <46003505+at-adorno@users.noreply.github.com>
2025-10-30 23:04:45 +0000

    config do banco

--- src/database/db.ts
+++ src/database/db.ts
@@ -15,6 +15,7 @@ const pool = new Pool({
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: Number(process.env.DB_PORT),
+    ssl: {rejectUnauthorized: false}
 });
 
 pool.on('connect', () => {
