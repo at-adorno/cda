37c462db096e56a227fb9cb8067877ee5977ed52
André Tavares Adorno <46003505+at-adorno@users.noreply.github.com>
2025-12-12 15:09:03 -0300

    feat: registrar rotas de Competência, Meta e PlanoCarreira em app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import usuariosRoutes from './routes/usuarioRoutes.ts';
 import cicloRoutes from './routes/cicloRoutes.ts';
 import colaboradorRoutes from './routes/colaboradorRoutes.ts';
+import competenciaRoutes from './routes/competenciaRoutes.ts';
+import metaRoutes from './routes/metaRoutes.ts';
+import planoCarreiraRoutes from './routes/planoCarreiraRoutes.ts';
 
 const app = express();
 app.use(express.json());
@@ -9,6 +12,9 @@ app.use(express.json());
 app.use('/api/usuarios', usuariosRoutes);
 app.use('/api/ciclos', cicloRoutes);
 app.use('/api/colaboradores', colaboradorRoutes);
+app.use('/api/competencias', competenciaRoutes);
+app.use('/api/metas', metaRoutes);
+app.use('/api/planoCarreiras', planoCarreiraRoutes);
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 4000;
 
