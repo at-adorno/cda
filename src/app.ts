531efb4ad510479a34b84cdf9e4568ef044a866d
André Tavares Adorno <46003505+at-adorno@users.noreply.github.com>
2025-12-12 10:52:29 -0300

    feat: adicionar imports e registros de rotas para ciclo e colaborador

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express';
-import usuariosRoutes from './routes/usuariosRoutes.ts';
+import usuariosRoutes from './routes/usuarioRoutes.ts';
+import cicloRoutes from './routes/cicloRoutes.ts';
+import colaboradorRoutes from './routes/colaboradorRoutes.ts';
 
 const app = express();
 app.use(express.json());
 
 app.use('/api/usuarios', usuariosRoutes);
+app.use('/api/ciclos', cicloRoutes);
+app.use('/api/colaboradores', colaboradorRoutes);
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 4000;
 
