8559cd9344c5793b59d403038f34a1fe59cce52e
André Tavares Adorno <46003505+at-adorno@users.noreply.github.com>
2025-12-12 11:13:39 -0300

    feat: adicionar CicloService com métodos CRUD

---
+++ src/services/CicloService.ts
@@ -0,0 +1,28 @@
+import CicloRepository from '../repositories/CicloRepository';
+import { Ciclo } from '../types/Ciclo';
+
+class CicloService {
+  private cicloRepository = new CicloRepository();
+
+  listarTodos(): Ciclo[] {
+    return this.cicloRepository.listar();
+  }
+
+  buscarPorId(id: number): Ciclo | undefined {
+    return this.cicloRepository.buscarPorId(id);
+  }
+
+  criar(ciclo: Omit<Ciclo, 'id'>): Ciclo {
+    return this.cicloRepository.criar(ciclo);
+  }
+
+  atualizar(id: number, ciclo: Partial<Omit<Ciclo, 'id'>>): Ciclo | undefined {
+    return this.cicloRepository.atualizar(id, ciclo);
+  }
+
+  remover(id: number): boolean {
+    return this.cicloRepository.remover(id);
+  }
+}
+
+export default new CicloService();
